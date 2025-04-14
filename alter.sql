e43f28f2057d544a6ed3005bd7d2744d7b585ce5
sowmik-sec <sowmik.sec@gmail.com>
2025-04-14 19:15:06 +0600

    Different Methods to Alter Tables For Primary key, Unique etc

--- alter.sql
+++ alter.sql
@@ -19,3 +19,11 @@ ALTER Table person2
 
 ALTER Table person2
     ALTER COLUMN user_age DROP NOT NULL;
+
+ALTER TABLE person2 
+    ADD constraint unique_person2_user_age UNIQUE(user_age);
+
+ALTER TABLE person2
+    DROP constraint unique_person2_user_age;
+
+TRUNCATE TABLE person3;
