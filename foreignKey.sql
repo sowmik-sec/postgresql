ec275b468c5d213248922e3368a46d381bfcac0b
sowmik-sec <sowmik.sec@gmail.com>
2025-04-15 11:52:41 +0600

    Enforcing Referential Integrity: Behaviors During Data Insertion

--- foreignKey.sql
+++ foreignKey.sql
@@ -7,5 +7,22 @@ CREATE TABLE "user"(
 CREATE Table post(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
-    user_id INTEGER REFERENCES "user"(id)
-)
+    user_id INTEGER REFERENCES "user"(id) NOT NULL
+);
+ALTER Table post
+alter COLUMN user_id set NOT NULL;
+
+
+INSERT INTO "user"(username) VALUES
+('akash'),
+('batash'),
+('sagor'),
+('nodi');
+INSERT INTO post(title, user_id) VALUES
+('Enjoying a sunny day with Akash!', 2),
+('Batash just shared a amazing recipe!',1),
+('Exploring adventures with Sagor', 4),
+('Nodi''s wisdom always leaves me inspired.',4);
+SELECT title, username from post
+JOIN "user"
+WHERE "user.id" = post.user_id;
