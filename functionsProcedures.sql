b0998a6bacc502f1ba6b4f9c9d0025a7fd9ea821
sowmik-sec <sowmik.sec@gmail.com>
2025-04-15 20:03:34 +0600

    Exploring Stored Procedure in PostgreSQL

--- functionsProcedures.sql
+++ functionsProcedures.sql
@@ -27,3 +27,32 @@ $$
 $$;
 
 SELECT delete_emp_by_id(3);
+
+CREATE Procedure remove_emp_var()
+LANGUAGE plpgsql
+as 
+$$
+    DECLARE
+    test_var INT;
+    BEGIN
+        SELECT employee_id INTO test_var FROM employees WHERE employee_id = 5;
+        DELETE FROM employees WHERE employee_id = test_var;
+    END
+$$;
+
+CALL remove_emp_var();
+
+CREATE Procedure remove_emp_by_id(p_emp_id int)
+LANGUAGE plpgsql
+as 
+$$
+    DECLARE
+    test_var INT;
+    BEGIN
+        SELECT employee_id INTO test_var FROM employees WHERE employee_id = p_emp_id;
+        DELETE FROM employees WHERE employee_id = test_var;
+        RAISE NOTICE 'Employee removed successfully!';
+    END
+$$;
+
+CALL remove_emp_by_id(7);
